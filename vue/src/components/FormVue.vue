c65fe90e56ba94c7e952be426a1c37fb59cc4fe9
NathPaiva <nathalia.piccoli@getninjas.com.br>
2017-12-06 11:13:01 -0200

    try test vue

--- vue/src/components/FormVue.vue
+++ vue/src/components/FormVue.vue
@@ -22,17 +22,20 @@ export default {
     }
   },
   created() {
-    fetch('https://gist.githubusercontent.com/nathpaiva/6e2bda071405e2e6711a642ff139dacf/raw/d6cf5ff96f8d3294972c5dbb24482d4d17a37da7/form-stepby')
-    // fetch('https://gist.githubusercontent.com/nathpaiva/6b1aad9203ab8c3abf97113c45e310ea/raw/5150fe9f8a2c9930ae02bb967408f45c5b2e1870/form-normal')
-      .then((data) => data.json())
-      .then((data) => {
-        this.data = data._embedded;
-        this.stepBy = data.type === 'stepby' ? true : false;
-        this.totalSteps = Object.keys(data._embedded).length;
-      })
-      .catch((error) => { console.log(error) });
+    this.getFormInfo();
   },
   methods: {
+    getFormInfo: function () {
+      // fetch('https://gist.githubusercontent.com/nathpaiva/6e2bda071405e2e6711a642ff139dacf/raw/d6cf5ff96f8d3294972c5dbb24482d4d17a37da7/form-stepby')
+      fetch('https://gist.githubusercontent.com/nathpaiva/6b1aad9203ab8c3abf97113c45e310ea/raw/5150fe9f8a2c9930ae02bb967408f45c5b2e1870/form-normal')
+        .then((data) => data.json())
+        .then((data) => {
+          this.data = data._embedded;
+          this.stepBy = data.type === 'stepby' ? true : false;
+          this.totalSteps = Object.keys(data._embedded).length;
+        })
+        .catch((error) => { console.log(error) });
+    },
     buttonOnClick: function() {
       if (this.step + 1 !== this.totalSteps) {
         ++this.step;
---
+++ vue/test/unit/specs/FormVue.spec.js
@@ -0,0 +1,141 @@
+import Vue from 'vue';
+import FormVue from '@/components/FormVue';
+import fetch from 'isomorphic-fetch';
+
+
+const data = {
+  "_embedded": {
+  "request_fields": [{
+      "reference": null,
+      "name": "Qual será o serviço?",
+      "label": "Qual será o serviço?",
+      "placeholder": "Qual será o serviço?",
+      "mask": "tipo de serviço",
+      "type": "enumerable",
+      "required": true,
+      "allow_multiple_value": true,
+      "allow_custom_value": false,
+      "values": {
+          "Coloração": "Coloração",
+          "Corte": "Corte",
+          "Escova": "Escova",
+          "Escova progressiva/definitiva": "Escova progressiva/definitiva",
+          "Luzes": "Luzes",
+          "Manicure": "Manicure",
+          "Pedicure": "Pedicure",
+          "Penteado": "Penteado"
+      },
+      "_embedded": {
+          "nested_fields": []
+      }
+  }, {
+      "reference": null,
+      "name": "Para quem será o serviço?",
+      "label": "Para quem será o serviço?",
+      "placeholder": "Para quem será o serviço?",
+      "mask": "indique para quem será o serviço",
+      "type": "enumerable",
+      "required": false,
+      "allow_multiple_value": true,
+      "allow_custom_value": false,
+      "values": {
+          "Criança": "Criança",
+          "Homem": "Homem",
+          "Mulher": "Mulher"
+      },
+      "_embedded": {
+          "nested_fields": []
+      }
+  }, {
+      "reference": null,
+      "name": "O serviço será para quantas pessoas?",
+      "label": "O serviço será para quantas pessoas?",
+      "placeholder": "O serviço será para quantas pessoas?",
+      "mask": "indique o número de pessoas",
+      "type": "enumerable",
+      "required": false,
+      "allow_multiple_value": false,
+      "allow_custom_value": false,
+      "values": {
+          "1": "1",
+          "2": "2",
+          "Mais de 3": "Mais de 3"
+      },
+      "_embedded": {
+          "nested_fields": []
+      }
+  }, {
+      "reference": null,
+      "name": "Para quando você precisa deste serviço?",
+      "label": "Para quando você precisa deste serviço?",
+      "placeholder": "Para quando você precisa deste serviço?",
+      "mask": "Indique o prazo do serviço",
+      "type": "enumerable",
+      "required": true,
+      "allow_multiple_value": false,
+      "allow_custom_value": false,
+      "values": {
+          "Hoje ou nos próximos dias": "Hoje ou nos próximos dias",
+          "Nos próximos 30 dias": "Nos próximos 30 dias",
+          "Nos próximos 3 meses": "Nos próximos 3 meses",
+          "Ainda não tenho previsão": "Ainda não tenho previsão"
+      },
+      "_embedded": {
+          "nested_fields": []
+      }
+  }, {
+      "name": "Informações Adicionais",
+      "label": "Informações Adicionais",
+      "type": "big_text",
+      "placeholder": "Descreva o que você precisa",
+      "required": false
+  }],
+  "user_fields": [{
+      "name": "lat_lng",
+      "label": "Em que bairro será o serviço?",
+      "type": "lat_lng",
+      "placeholder": "",
+      "required": true
+  }, {
+      "name": "name",
+      "label": "Nome",
+      "type": "small_text",
+      "placeholder": "",
+      "required": true
+  }, {
+      "name": "email",
+      "label": "Email",
+      "type": "email",
+      "placeholder": "Ex: nome@email.com",
+      "required": true
+  }, {
+      "name": "phone",
+      "label": "Celular",
+      "type": "phone",
+      "placeholder": "",
+      "required": true
+  }]
+}};
+
+describe('FormVue.vue', () => {
+  it('has a created hook', () => {
+    expect(typeof FormVue.created).toBe('function');
+  });
+
+  it('sets the correct default data', () => {
+    expect(typeof FormVue.data).toBe('function');
+    const defaultData = FormVue.data();
+    expect(defaultData.data).toEqual({});
+  });
+
+  it('check if form is redering form', (done) => {
+    const vm = new Vue(FormVue).$mount();
+    expect(vm.$el).toMatchSnapshot();
+    Vue.nextTick(() => {
+      // console.log("vm.$el", vm.$el);
+      // expect(vm.$el.textContent).toBe('foo')
+      expect(true).toBe(true);
+      done();
+    });
+  });
+});
