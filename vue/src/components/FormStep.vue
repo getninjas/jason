73ef45291e0730a2f21eb48dbfbcde2f63423136
NathPaiva <nathalia.piccoli@getninjas.com.br>
2017-12-06 12:17:06 -0200

    create require attr, change name of label

--- vue/src/components/FormStep.vue
+++ vue/src/components/FormStep.vue
@@ -2,8 +2,8 @@
   <div>
     <h1>Step {{position}}</h1>
     <div v-bind:key='i' v-for="(field, i) in value">
-      <label v-bind:for="field.name">{{field.name}}</label><br>
-      <input type="text" v-bind:id="field.name" v-bind:name="field.name" v-model="formData[field.name]" v-bind:placeholder="field.placeholder">
+      <label v-bind:for="field.name">{{field.label}}</label><br>
+      <input type="text" v-bind:id="field.name" v-bind:name="field.name" required="field.required" v-model="formData[field.name]" v-bind:placeholder="field.placeholder">
     </div>
 
     <button type="button" v-if="stepBy && step !== 0" v-on:click="onBackClick">Back</button>
--- vue/src/components/FormVue.vue
+++ vue/src/components/FormVue.vue
@@ -26,8 +26,8 @@ export default {
   },
   methods: {
     getFormInfo: function () {
-      // fetch('https://gist.githubusercontent.com/nathpaiva/6e2bda071405e2e6711a642ff139dacf/raw/d6cf5ff96f8d3294972c5dbb24482d4d17a37da7/form-stepby')
-      fetch('https://gist.githubusercontent.com/nathpaiva/6b1aad9203ab8c3abf97113c45e310ea/raw/5150fe9f8a2c9930ae02bb967408f45c5b2e1870/form-normal')
+      fetch('https://gist.githubusercontent.com/nathpaiva/6e2bda071405e2e6711a642ff139dacf/raw/d6cf5ff96f8d3294972c5dbb24482d4d17a37da7/form-stepby')
+      // fetch('https://gist.githubusercontent.com/nathpaiva/6b1aad9203ab8c3abf97113c45e310ea/raw/5150fe9f8a2c9930ae02bb967408f45c5b2e1870/form-normal')
         .then((data) => data.json())
         .then((data) => {
           this.data = data._embedded;
