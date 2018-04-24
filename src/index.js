b3ffb8324ab61a16bdd040e6a7f957a1b5d4e2a5
Felipe Orlando <fobsouza@gmail.com>
2018-04-24 11:48:21 -0300

    Remove console logs

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import App from './App';
 export default class Jason {
   constructor(options) {
     Object.assign(this, options);
-    console.log('construct!');
   }
 
   init() {
-    console.log('init!');
     ReactDOM.render(<App />, this.element);
   }
 }
