a5447acb6c1180db934fc61bff2b2fa6d2266f43
Felipe Orlando <fobsouza@gmail.com>
2018-04-24 11:06:41 -0300

    Export Jason on source index

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default class Jason {
+  constructor(options) {
+    Object.assign(this, options);
+    console.log('construct!');
+  }
+
+  init() {
+    console.log('init!');
+    ReactDOM.render(<App />, this.element);
+  }
+}
