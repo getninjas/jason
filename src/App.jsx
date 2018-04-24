7da7ab07962e47d352c98701388613926e402123
Felipe Orlando <fobsouza@gmail.com>
2018-04-24 15:54:00 -0300

    Change word to run ci

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Jason!</h1>
         </header>
 
         <StartText name="Ion" />
