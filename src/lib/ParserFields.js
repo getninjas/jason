99ec4b4da7c433f697d1444f76825603807ab176
Felipe Orlando <fobsouza@gmail.com>
2018-04-25 16:03:59 -0300

    Spec for ParserFields

--- src/lib/ParserFields.js
+++ src/lib/ParserFields.js
@@ -1,11 +1,37 @@
 export default class ParserFields {
   constructor(json) {
-    this.json = JSON.parse(json);
+    this.json = json;
   }
 
   init() {
-    const fields = this.json.data.form.fields.edges;
+    this._extractData();
+  }
 
-    fields.map((field) => field.node);
+  _extractData() {
+    const fields = this.json.data.category.form.fields.edges;
+
+    const formatedFields = fields.map((field) => {
+      const { title, values, placeholder, type, required } = field.node;
+
+      const formatedValues = this._extractChildrenValues(values);
+
+      return {
+        title,
+        values: formatedValues,
+        placeholder,
+        type,
+        required,
+      };
+    });
+
+    return formatedFields;
+  }
+
+  _extractChildrenValues(values) {
+    return values.edges.map((item) => {
+      const { databaseId, value } = item.node;
+
+      return { databaseId, value };
+    });
   }
 }
