9023ace82aab37f62943ad07f113e4bc0ef14a62
Felipe Orlando <fobsouza@gmail.com>
2018-04-26 16:59:51 -0300

    Ad spec for Form component

---
+++ spec/components/Form.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Form from '../../src/components/Form';
+import expectedFields from '../fixture/expected-fields.json';
+
+describe('Form', () => {
+  it('renders custom props', () => {
+    const component = renderer.create(
+      <Form name={'form'} formatedJSON={expectedFields} />,
+    );
+
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders defaultProps', () => {
+    const component = renderer.create(
+      <Form name={'form'} formatedJSON={expectedFields} />,
+    );
+
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
