93feba4ddc107172ce5d3c3fee6c01c53cde19c4
Felipe Orlando <fobsouza@gmail.com>
2018-04-30 11:23:34 -0300

    Create Breadcrumb

---
+++ src/components/Breadcrumb.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Breadcrumb extends React.Component {
+  static defaultProps = {
+    steps: []
+  }
+
+  render() {
+    return (
+      <div className="breadcrumb">
+        <ol className="form__steps space-box-medium">
+          {
+            this.props.steps.map((step, index) => {
+              return (
+                <li key={`breadcrumb-${index}`} className="form__steps-item form__steps-item--active active">
+                  {step.title}
+                </li>
+              );
+            })
+          }
+        </ol>
+      </div>
+    );
+  }
+}
+
+Breadcrumb.propTypes = {
+  steps: PropTypes.array.isRequired,
+}
