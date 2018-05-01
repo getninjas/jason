e552bfbbdf901dad6163034b69fcb1fbfcc8e55d
Felipe Orlando <fobsouza@gmail.com>
2018-05-01 11:05:39 -0300

    Adds breadcrumb spec to test active breacrumb

--- spec/components/Breadcrumb.spec.js
+++ spec/components/Breadcrumb.spec.js
@@ -23,4 +23,18 @@ describe('Breadcrumb', () => {
     expect(tree).toMatchSnapshot();
   });
 
+  describe('when second step is active', () => {
+    it('expects the second breadcrumb to be active', () => {
+      const component = shallow(<Breadcrumb active={1} steps={data.form.steps} />);
+
+      expect(component.find('li').last().hasClass('form__steps-item--active')).toBe(true);
+    });
+
+    it('expects the first breadcrumb to be inactive', () => {
+      const component = shallow(<Breadcrumb active={1} steps={data.form.steps} />);
+
+      expect(component.find('li').first().hasClass('form__steps-item--active')).toBe(false);
+    });
+  });
+
 });
