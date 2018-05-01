3c40da60b3d7dfaa8867f770a2c29a16f3655c08
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-05-01 09:40:42 -0300

    extracted styling logic

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class Breadcrumb extends React.Component {
+  isActive(index) {
+    return this.props.active === index;
+  }
+
+  handleActiveSyle(index) {
+    return this.isActive(index)? 'form__steps-item--active' : '';
+  }
+
   render() {
     return (
       <ol className="form__steps space-box-medium">
         {
           this.props.steps.map((step, index) => {
             return (
-              <li key={`breadcrumb-${index}`} className={`form__steps-item ${this.props.active === index ? 'form__steps-item--active' : ''}`}>
+              <li key={`breadcrumb-${index}`} className={`form__steps-item ${this.handleActiveSyle(index)}`}>
                 {step.breadcrumbTitle}
               </li>
             );
