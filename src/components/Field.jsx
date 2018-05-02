dac150f3c20b45e41c84ee32c74c064d6504d912
Felipe Orlando <fobsouza@gmail.com>
2018-05-02 10:29:18 -0300

    Separate wrapper and span style from tags

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Field extends Component {
+  constructor() {
+    super();
+
+    this.wrapperStyle = 'form__field form__field--fluid input';
+    this.spanStyle = 'form__message form__message--invalid space-element-small error';
+  }
+
   render() {
     return (
-      <div className="form__field form__field--fluid input">
+      <div className={this.wrapperStyle}>
         <label htmlFor={this.props.id} className="form__label">
           { this.props.label }
         </label>
 
         { this.props.children }
 
-        <span className="form__message form__message--invalid space-element-small error">
+        <span className={this.spanStyle}>
           { this.props.errorMessage }
         </span>
       </div>
