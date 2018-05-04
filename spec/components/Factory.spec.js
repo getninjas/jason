c7f7d365603c25f13320c48d95a5982734cc7a84
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-05-04 17:04:13 -0300

    fixed Factory spec

--- spec/components/Factory.spec.js
+++ spec/components/Factory.spec.js
@@ -5,37 +5,42 @@ const select = form.steps[0].fields[0];
 const textArea = form.steps[0].fields[2];
 const input = form.steps[0].fields[1];
 
+const commonProps = {
+  onFieldChange: ()=> {},
+  formName: 'formNameTest',
+  index: 1,
+};
+
 describe('Factory', () => {
   it('renders select', () => {
-    const result = Factory.getComponent(select, 1);
+    const result = Factory.getComponent({ item: select, ...commonProps });
 
     expect(result.type.name).toEqual('Select');
   });
 
   it('renders textarea', () => {
-    const result = Factory.getComponent(textArea, 1);
+    const result = Factory.getComponent({ item: textArea, ...commonProps });
 
     expect(result.type.name).toEqual('TextArea');
   });
 
-  it('renders input on text', () => {
-    const result = Factory.getComponent(input, 1);
+  it('renders input type text', () => {
+    const result = Factory.getComponent({ item: input, ...commonProps });
 
     expect(result.type.name).toEqual('Input');
   });
 
-  it('renders input on phone', () => {
+  it('renders input type phone', () => {
     const phone = Object.assign({}, input, { type: 'phone' });
-
-    const result = Factory.getComponent(phone, 1);
+    const result = Factory.getComponent({ item: phone, ...commonProps });
 
     expect(result.type.name).toEqual('Input');
   });
 
-  it('renders input on email', () => {
+  it('renders input type email', () => {
     const email = Object.assign({}, input, { type: 'email' });
 
-    const result = Factory.getComponent(email, 1);
+    const result = Factory.getComponent({ item: email, ...commonProps });
 
     expect(result.type.name).toEqual('Input');
   });
@@ -44,7 +49,7 @@ describe('Factory', () => {
     const invalidArg = Object.assign({}, input, { type: 'xpto' });
 
     expect(() => {
-      Factory.getComponent(invalidArg, 1)
+      Factory.getComponent({ item: invalidArg, ...commonProps })
     }).toThrow('Invalid argument type');
   });
 });
