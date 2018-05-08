089435eaa138f9f65f3cce0ec175ecb145613a3e
William Costa <william.costa@getninjas.com.br>
2018-05-08 11:19:05 -0300

    Add spec to isMinLength method

--- spec/components/Form/validation.spec.js
+++ spec/components/Form/validation.spec.js
@@ -1,4 +1,4 @@
-import { isEmpty } from '../../../src/components/Form/validation';
+import { isEmpty, isMinLength } from '../../../src/components/Form/validation';
 
 describe('.isEmpty', () => {
   it('returns true for empty string', () => {
@@ -11,5 +11,21 @@ describe('.isEmpty', () => {
     const result = isEmpty('xpto');
 
     expect(result).toBe(false);
+  });
+});
+
+describe('.isMinLength', () => {
+  it('returns false for string greather than minLength', () => {
+    const minLength = 3;
+    const result = isMinLength('xpto', minLength);
+
+    expect(result).toBe(false);
+  });
+
+  it('returns true for string smaller than minLength', () => {
+    const minLength = 3;
+    const result = isMinLength('xp', minLength);
+
+    expect(result).toBe(true);
   });
 });
