e0e0077dc5dd6d1eb79b832317c99f579b77c6de
William Costa <william.costa@getninjas.com.br>
2018-05-09 13:40:51 -0300

    Create Snapshot to zipcode component and update Form and Step Snapshots.

---
+++ spec/components/Zipcode.spec.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Zipcode from '../../src/components/Zipcode';
+import { enzymeConfig, shallow } from '../enzymeConfig';
+
+enzymeConfig();
+
+describe('Zipcode', () => {
+  it('renders defaultProps', () => {
+    const component = renderer.create(
+      <Zipcode
+        type={'zipcode'}
+        key={`zipcode-1`}
+        id={'zipcodeTest'}
+        name={'zipcodeTest'}
+        placeholder={'00000-000'}
+      />,
+    );
+
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
