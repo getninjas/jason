e0cc361054cf5422b671f28bf6d435ee39e14119
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-05-14 09:21:34 -0300

    fixed spec

--- src/components/__mocks__/axios.js
+++ src/components/__mocks__/axios.js
@@ -1,6 +1,6 @@
 export default {
   get: jest.fn((url) => {
-    if(url==='http://unknowservice/04707060/json') {
+    if(url==='http://unknowservice/04707060') {
       return Promise.reject({ data: {}});
     }
     return Promise.resolve({ data: { type_street: '', street: 'Rua Mock', city: 'Cidade Mock', neighborhood: 'Bairro Mock', uf: 'SP' }});
