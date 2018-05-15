5ed2a5bb26b49fc658966c85d79a2346b7afede5
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-05-15 17:41:48 -0300

    added submit hooks

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import Form from './components/Form';
 
 export default class App extends Component {
   render() {
-    return <Form name={'jason-form'} action={'/'} data={this.props.data} />;
+    return <Form {...this.props} data={this.props.data} />;
   }
 }
 
 App.propTypes = {
   data: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  action: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func,
+  onSubmitSuccess: PropTypes.func,
+  onSubmitError: PropTypes.func,
 };
