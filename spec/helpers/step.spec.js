d77be907e0333307a9fb827a9ad416811f262cd8
William Costa <dev.williamcosta@gmail.com>
2018-05-15 12:45:32 -0300

    Add spec to addHeaderMarkup method

--- spec/helpers/step.spec.js
+++ spec/helpers/step.spec.js
@@ -1,4 +1,4 @@
-import { display } from '../../src/helpers/step';
+import { display, addHeaderMarkup } from '../../src/helpers/step';
 
 describe('.display', () => {
   it('returns none to receive false', () => {
@@ -11,5 +11,23 @@ describe('.display', () => {
     const result = display(true);
 
     expect(result).toEqual('block');
+  });
+});
+
+describe('.addHeaderMarkup', () => {
+  it('returns header markup', () => {
+    const headerHTML = '<h2 class="widget__title section-title--high-contrast">Lorem Ipsum is simply dummy text <small>It has survived not</small></h2>';
+
+    const result = addHeaderMarkup(headerHTML);
+
+    expect(result.props.className).toEqual('__headerMarkup__');
+  });
+
+  it('does not add header markup', () => {
+    const headerHTML = '';
+
+    const result = addHeaderMarkup(headerHTML);
+
+    expect(result).toEqual('');
   });
 });
