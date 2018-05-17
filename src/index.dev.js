9ab1f5aa770ee4a080ea29c509a8719eb68f04b7
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-05-17 09:06:10 -0300

    updated dev hooks

--- src/index.dev.js
+++ src/index.dev.js
@@ -8,10 +8,10 @@ ReactDOM.render(
   name='form-name'
   action='http://www.mocky.io/v2/5afb459c2f00005b00f7c7ab'
   data={form}
-  onZipcodeFetchSuccess={ data => console.log('App onZipcodeFetchSuccess', data) }
-  onZipcodeFetchError={ data => console.log('App onZipcodeFetchError', data) }
-  onSubmit={ () => console.log('App onSubmit') }
-  onSubmitSuccess={ () => console.log('App onSubmitSuccess') }
-  onSubmitError={ () => console.log('App onSubmitError') }
-  onStepChange={ () => console.log('App onStepChange') } />,
+  onZipcodeFetchSuccess={ data => console.log('App Index Dev onZipcodeFetchSuccess', data) }
+  onZipcodeFetchError={ data => console.log('App Index Dev onZipcodeFetchError', data) }
+  onSubmit={ () => console.log('App Index Dev onSubmit') }
+  onSubmitSuccess={ () => console.log('App Index Dev onSubmitSuccess') }
+  onSubmitError={ () => console.log('App Index Dev onSubmitError') }
+  onStepChange={ () => console.log('App Index Dev onStepChange') } />,
   document.getElementById('root'));
