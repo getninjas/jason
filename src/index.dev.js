e96b0cb0993c733881ddc064c7dcd59ee962fd2a
NathPaiva <hello@nathpaiva.com.br>
2018-05-25 14:43:05 -0300

    Remove some spaces on methods

--- src/index.dev.js
+++ src/index.dev.js
@@ -8,12 +8,12 @@ ReactDOM.render(
   name='form-name'
   action='http://www.mocky.io/v2/5afb459c2f00005b00f7c7ab'
   data={form}
-  onReady={ () => console.log('App Index Dev onReady') }
-  onZipcodeFetchSuccess={ data => console.log('App Index Dev onZipcodeFetchSuccess', data) }
-  onZipcodeFetchError={ data => console.log('App Index Dev onZipcodeFetchError', data) }
-  onSubmit={ () => console.log('App Index Dev onSubmit') }
-  onSubmitSuccess={ data => console.log('App Index Dev onSubmitSuccess') }
-  onSubmitFieldError={ data => console.log('App Index Dev onSubmitFieldError', data) }
-  onSubmitError={ data => console.log('App Index Dev onSubmitError') }
-  onStepChange={ () => console.log('App Index Dev onStepChange') } />,
+  onReady={() => console.log('App Index Dev onReady') }
+  onZipcodeFetchSuccess={data => console.log('App Index Dev onZipcodeFetchSuccess', data) }
+  onZipcodeFetchError={data => console.log('App Index Dev onZipcodeFetchError', data) }
+  onSubmit={() => console.log('App Index Dev onSubmit') }
+  onSubmitSuccess={data => console.log('App Index Dev onSubmitSuccess') }
+  onSubmitFieldError={data => console.log('App Index Dev onSubmitFieldError', data) }
+  onSubmitError={data => console.log('App Index Dev onSubmitError') }
+  onStepChange={() => console.log('App Index Dev onStepChange') } />,
   document.getElementById('root'));
