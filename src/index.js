669c445e7be3f3bca5adca6a3a62f8093a7d335c
NathPaiva <hello@nathpaiva.com.br>
2018-05-25 14:59:16 -0300

    Revert "Remove some spaces on methods"
    This reverts commit e96b0cb0993c733881ddc064c7dcd59ee962fd2a.

--- src/index.js
+++ src/index.js
@@ -18,14 +18,14 @@ export default class Jason {
       <Form data={this.data.form}
         name={this.name}
         action={this.action}
-        onReady={this.onReady}
-        onZipcodeFetchSuccess={(data) => { this.onZipcodeFetchSuccess(data); }}
-        onZipcodeFetchError={(data) => { this.onZipcodeFetchError(data); }}
-        onSubmit={this.onSubmit}
-        onSubmitSuccess={this.onSubmitSuccess}
-        onSubmitFieldError={this.onSubmitFieldError}
-        onSubmitError={this.onSubmitError}
-        onStepChange={this.onStepChange}
+        onReady={ this.onReady }
+        onZipcodeFetchSuccess={ (data) => { this.onZipcodeFetchSuccess(data); } }
+        onZipcodeFetchError={ (data) => { this.onZipcodeFetchError(data); } }
+        onSubmit={ this.onSubmit }
+        onSubmitSuccess={ this.onSubmitSuccess }
+        onSubmitFieldError={ this.onSubmitFieldError }
+        onSubmitError={ this.onSubmitError }
+        onStepChange={ this.onStepChange }
         ref={(component) => { this.form = component; }} />,
         this.element,
     );
