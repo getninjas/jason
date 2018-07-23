8fb63c95c5b243208c618bd2581fc0b97448de24
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-07-23 08:47:37 -0300

    removed unnecessary code

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,26 +13,12 @@ const defaultProps = {
 };
 
 export default class Button extends Component {
-  constructor() {
-    super();
-
-    this.click = this.click.bind(this);
-  }
-
-  click(evt) {
-    evt.preventDefault();
-
-    if (evt.nativeEvent.isTrusted) {
-      this.props.handleButtonClick(evt);
-    }
-  }
-
   render() {
     const type = this.props.isSubmit ? 'submit' : 'button';
     const style = 'btn btn--regular btn--high-contrast btn--fluid space-box-medium';
 
     return (
-      <button type={type} className={style} onClick={this.click}>
+      <button type={type} className={style} onClick={this.props.handleButtonClick}>
         {this.props.children}
       </button>
     );
