dbae083ce72fcf410cc4fb43a122c1e457a096c7
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-07-23 11:34:38 -0300

    update fields helper

--- spec/helper.js
+++ spec/helper.js
@@ -1,8 +1,26 @@
+const validFields = {
+  text: '111111',
+  textarea: '111111',
+  zipcode: '11111-222',
+  email: 'jondoe@xpto.com',
+  phone: '(11) 99999-8888',
+  select: 7117,
+  checkbox: ['6792'],
+};
+
 const fillFormFields = (steps) => {
   return [...steps].map((step) => {
     const fields = step.fields.map((field) => {
       const updatedField = Object.assign({}, field);
-      updatedField.value = '1111111';
+
+      Object.entries(validFields).map((entry) => {
+        const [key, value] = entry;
+        if (updatedField.type === key) {
+          Object.assign(updatedField, { value });
+        }
+
+        return updatedField;
+      });
 
       return updatedField;
     });
