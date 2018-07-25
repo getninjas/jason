a45c09b6ea8213268ca9c401d288bec09756343d
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-07-25 09:18:05 -0300

    linting

--- src/components/Zipcode.js
+++ src/components/Zipcode.js
@@ -141,10 +141,10 @@ export default class Zipcode extends Component {
 
     return (
       <AppContext.Consumer>
-        {context => <Fragment>
+        { context => <Fragment>
           <a href='http://www.buscacep.correios.com.br' target='_blank' className='form__label-link' rel='noopener noreferrer'>Não lembra seu CEP?</a>
           <input id={id} name={name} className={style} type='tel' placeholder={placeholder} required={required} onChange={this.onChange.bind(this, context.onZipcodeFetchSuccess, context.onZipcodeFetchError)} onBlur={this.onBlur.bind(this, context.onZipcodeFetchSuccess, context.onZipcodeFetchError)} ref={this.inputRef} value={this.state.value} />
-          {fetching ? <span className='zipcode__loader' >Buscando CEP...</span> : <span className='full-address'>{fullAddress}</span>}
+          { fetching ? <span className='zipcode__loader' >Buscando CEP...</span> : <span className='full-address'>{fullAddress}</span> }
           <input id='street' name='street' type='hidden' value={street} />
           <input id='neighborhood' name='neighborhood' type='hidden' value={neighborhood} />
           <input id='city' name={'city'} type={'hidden'} value={city} />
