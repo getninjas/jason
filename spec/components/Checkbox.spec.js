f789d4c9f58a7ac62c588eb65c85f297b1240c33
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-07-25 08:43:40 -0300

    fixed specs

--- spec/components/Checkbox.spec.js
+++ spec/components/Checkbox.spec.js
@@ -44,91 +44,91 @@ describe('Checkbox', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  // it('changes input checked .onChage event', () => {
-  //   const onChange = jest.fn();
+  it('changes input checked .onChage event', () => {
+    const onChange = jest.fn();
 
-  //   const wrapper = mount(
-  //     <Checkbox
-  //       {...commonProps}
-  //       values={[checkbox.values[0]]}
-  //       onFieldChange={onChange}
-  //     />,
-  //   );
+    const wrapper = mount(
+      <Checkbox
+        {...commonProps}
+        values={[checkbox.values[0]]}
+        onFieldChange={onChange}
+      />,
+    );
 
-  //   expect(wrapper.instance().inputChecked).toEqual({});
+    expect(wrapper.instance().inputChecked).toEqual({});
 
-  //   wrapper.find('input').simulate('change', { target: { checked: true, id: 6792, getAttribute: () => 'Cozinhar' } });
+    wrapper.find('input').simulate('change', { target: { checked: true, id: 6792, getAttribute: () => 'Cozinhar' } });
 
-  //   expect(wrapper.instance().props.onFieldChange).toBeCalled();
-  //   expect(wrapper.instance().inputChecked).toEqual({ 6792: 6792 });
-  // });
+    expect(wrapper.instance().props.onFieldChange).toBeCalled();
+    expect(wrapper.instance().inputChecked).toEqual({ 6792: 6792 });
+  });
 
-  // it('changes input checked .onBlurCheckbox event', () => {
-  //   const onBlur = jest.fn();
+  it('changes input checked .onBlurCheckbox event', () => {
+    const onBlur = jest.fn();
 
-  //   const wrapper = mount(
-  //     <Checkbox
-  //       {...commonProps}
-  //       values={[checkbox.values[0]]}
-  //       onFieldBlur={onBlur}
-  //     />,
-  //   );
+    const wrapper = mount(
+      <Checkbox
+        {...commonProps}
+        values={[checkbox.values[0]]}
+        onFieldBlur={onBlur}
+      />,
+    );
 
-  //   wrapper.find('input').simulate('blur');
+    wrapper.find('input').simulate('blur');
 
-  //   expect(wrapper.instance().props.onFieldBlur).toBeCalled();
-  // });
+    expect(wrapper.instance().props.onFieldBlur).toBeCalled();
+  });
 
-  // describe('with input text', () => {
-  //   it('render component', () => {
-  //     const options = { createNodeMock };
-  //     const component = renderer.create(
-  //       <Checkbox
-  //         {...commonProps}
-  //         values={[checkbox.values[5]]}
-  //       />, options,
-  //     );
+  describe('with input text', () => {
+    it('render component', () => {
+      const options = { createNodeMock };
+      const component = renderer.create(
+        <Checkbox
+          {...commonProps}
+          values={[checkbox.values[5]]}
+        />, options,
+      );
 
-  //     const tree = component.toJSON();
+      const tree = component.toJSON();
 
-  //     expect(tree).toMatchSnapshot();
-  //   });
+      expect(tree).toMatchSnapshot();
+    });
 
-  //   it('checkbox with OTHER value need to have an input text', () => {
-  //     const onBlur = jest.fn();
+    it('checkbox with OTHER value need to have an input text', () => {
+      const onBlur = jest.fn();
 
-  //     const wrapper = mount(
-  //       <Checkbox
-  //         {...commonProps}
-  //         values={[checkbox.values[5]]}
-  //         onFieldChange={onBlur}
-  //       />,
-  //     );
+      const wrapper = mount(
+        <Checkbox
+          {...commonProps}
+          values={[checkbox.values[5]]}
+          onFieldChange={onBlur}
+        />,
+      );
 
-  //     expect(wrapper.instance().inputChecked).toEqual({});
-  //     expect(wrapper.find('input[type="text"]').get(0).props.disabled).toBeTruthy();
+      expect(wrapper.instance().inputChecked).toEqual({});
+      expect(wrapper.find('input[type="text"]').get(0).props.disabled).toBeTruthy();
 
-  //     wrapper.find('input[type="checkbox"]').simulate('change', { target: { checked: true, id: 7639, getAttribute: () => 'OTHER' } });
+      wrapper.find('input[type="checkbox"]').simulate('change', { target: { checked: true, id: 7639, getAttribute: () => 'OTHER' } });
 
-  //     expect(wrapper.instance().props.onFieldChange).toBeCalled();
-  //     expect(wrapper.find('input[type="text"]').get(0).props.disabled).toBeFalsy();
-  //     expect(wrapper.instance().inputChecked).toEqual({ 7639: '' });
-  //   });
+      expect(wrapper.instance().props.onFieldChange).toBeCalled();
+      expect(wrapper.find('input[type="text"]').get(0).props.disabled).toBeFalsy();
+      expect(wrapper.instance().inputChecked).toEqual({ 7639: '' });
+    });
 
-  //   it('checkbox with OTHER value need to have an input text', () => {
-  //     const wrapper = mount(
-  //       <Checkbox
-  //       {...commonProps}
-  //       values={[checkbox.values[5]]}
-  //       />,
-  //     );
+    it('checkbox with OTHER value need to have an input text', () => {
+      const wrapper = mount(
+        <Checkbox
+        {...commonProps}
+        values={[checkbox.values[5]]}
+        />,
+      );
 
-  //     expect(wrapper.instance().inputChecked).toEqual({});
+      expect(wrapper.instance().inputChecked).toEqual({});
 
-  //     wrapper.find('input[type="checkbox"]').simulate('change', { target: { checked: true, id: 7639, getAttribute: () => 'OTHER' } });
-  //     wrapper.find('input[type="text"]').simulate('blur', { target: { value: 'Teste value', getAttribute: () => 7639 } });
+      wrapper.find('input[type="checkbox"]').simulate('change', { target: { checked: true, id: 7639, getAttribute: () => 'OTHER' } });
+      wrapper.find('input[type="text"]').simulate('blur', { target: { value: 'Teste value', getAttribute: () => 7639 } });
 
-  //     expect(wrapper.instance().inputChecked).toEqual({ 7639: 'Teste value' });
-  //   });
-  // });
+      expect(wrapper.instance().inputChecked).toEqual({ 7639: 'Teste value' });
+    });
+  });
 });
