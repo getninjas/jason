b23d3392c9c4c848526bbe115f0bddfb0a62235a
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2018-10-30 09:33:24 -0300

    handle empty address fields

--- src/helpers/zipcode.js
+++ src/helpers/zipcode.js
@@ -15,9 +15,12 @@ export const getEmptyState = state =>
     return { ...output, [key]: defaultValue };
   }, {});
 
-const getFullAddress = ({ street, neighborhood, city, uf }) =>
-  `${street}, ${neighborhood} \n${city} - ${uf}`;
+const getFullAddress = ({ street, neighborhood, city, uf }) => {
+  const displayStreet = street ? `${street},` : '';
+  const displayNeighborhood = neighborhood ? `${neighborhood} \n` : '';
 
+  return `${displayStreet}${displayNeighborhood}${city} - ${uf}`;
+};
 
 export const fillAddressState = (responseAddress, zipcode) => {
   const result = Object.keys(responseAddress)
