99ee0651ab23f90227b134b005a613671b0d83e2
Ion D. Filho <iondrimba@gmail.com>
2019-01-16 14:14:40 -0200

    emulate prod impl

--- src/index.dev.js
+++ src/index.dev.js
@@ -1,19 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form';
-import { form } from './form.json';
+import { form } from './form.dev.json';
 
-ReactDOM.render(
-  <Form
-  name='form-name'
-  action='http://www.mocky.io/v2/5afb459c2f00005b00f7c7ab'
-  data={form}
-  onReady={ () => {} }
-  onZipcodeFetchSuccess={ data => data }
-  onZipcodeFetchError={ data => data }
-  onSubmit={ () => {} }
-  onSubmitSuccess={ data => data }
-  onSubmitFieldError={ data => data }
-  onSubmitError={ data => data }
-  onStepChange={ () => {} } />,
-  document.getElementById('root'));
+class Jason {
+  constructor(options) {
+    Object.assign(this, options);
+
+    this.form = null;
+  }
+
+  post() {
+    this.form.formSubmit();
+  }
+
+  updateFields(fields) {
+    this.form.updateFields(fields);
+  }
+
+  init() {
+    ReactDOM.render(
+      <Form data={this.data.form}
+        name={this.name}
+        action={this.action}
+        onReady={this.onReady}
+        onZipcodeFetchSuccess={(data) => { this.onZipcodeFetchSuccess(data); }}
+        onZipcodeFetchError={(data) => { this.onZipcodeFetchError(data); }}
+        onSubmit={this.onSubmit}
+        onSubmitSuccess={this.onSubmitSuccess}
+        onSubmitFieldError={this.onSubmitFieldError}
+        onSubmitError={this.onSubmitError}
+        onStepChange={this.onStepChange}
+        ref={(component) => { this.form = component; }} />,
+      this.element,
+    );
+  }
+}
+
+
+const jason = new Jason({
+  name: 'form-name',
+  action: 'http://www.mocky.io/v2/5afb459c2f00005b00f7c7ab',
+  data: { form },
+  onReady: () => {},
+  onZipcodeFetchSuccess: data => data,
+  onZipcodeFetchError: data => data,
+  onSubmit: () => {},
+  onSubmitSuccess: data => data,
+  onSubmitFieldError: data => data,
+  onSubmitError: data => data,
+  onStepChange: () => {},
+  element: document.getElementById('root'),
+});
+
+jason.init();
