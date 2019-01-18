0a55c3bb82cf62f866af7176142500b27c790f6b
Ion D. Filho <iondrimba@gmail.com>
2019-01-18 18:09:28 -0200

    add UMD export option

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -160,6 +160,8 @@ module.exports = function(webpackEnv) {
       // We inferred the "public path" (such as / or /my-project) from homepage.
       // We use "/" in development.
       publicPath: publicPath,
+      library: 'jason',
+      libraryTarget: 'umd',
       // Point sourcemap entries to original disk location (format as URL on Windows)
       devtoolModuleFilenameTemplate: isEnvProduction
         ? info =>
