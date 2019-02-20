4b08f0205cfdafc9c49c07f935bc026d74c1d367
Ion D. Filho <iondrimba@gmail.com>
2019-02-20 16:59:25 -0300

    Add async step hook

---
+++ build/precache-manifest.b81866280c23ca08a9a0cd5475fadb63.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "b53a6d8a5ca93f7d8fee",
+    "url": "/static/js/main.b53a6d8a.js"
+  },
+  {
+    "revision": "dd4fc5ad06dfbcaf10e9f403717357d4",
+    "url": "/index.html"
+  }
+];
