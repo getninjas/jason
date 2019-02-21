b1c25ca646cb1bc80acf3225038e2fc5aad411bf
William Costa <dev.williamcosta@gmail.com>
2019-02-21 15:41:26 -0300

    Fix build package

---
+++ build/precache-manifest.0b6e1ac27a4755ebc13cf3c93e3dd2f9.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eb5b31de92f7493a20c",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "f12b5d1e94a165311e8ae5fe411d4ba0",
+    "url": "/index.html"
+  }
+];
