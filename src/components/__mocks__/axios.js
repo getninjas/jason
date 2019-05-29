cf0d7bf08cbaeb1197e7f6e0233966214fb1af8e
Eve Montalvão <evemontalvao@gmail.com>
2019-05-29 17:32:15 -0300

    Add spec to promise rejection case on submit

--- src/components/__mocks__/axios.js
+++ src/components/__mocks__/axios.js
@@ -6,6 +6,18 @@ export default {
     return Promise.resolve({ data: { type_street: '', street: 'Rua Mock', city: 'Cidade Mock', neighborhood: 'Bairro Mock', uf: 'SP' } });
   }),
   post: jest.fn((url, body) => {
+    if (!url.length) {
+      const error = new Error({
+        config: {},
+        data: '',
+        headers: {},
+        request: {},
+        status: 404,
+        statusText: 'Not Found',
+      });
+
+      return Promise.reject(error);
+    }
     return Promise.resolve({ data: { link: 'https://testdomain.com' } });
   }),
 };
