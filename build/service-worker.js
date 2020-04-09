11a864d65ae13c3029cbd0fe7bfcd173de322c2a
Jose Luiz Coe <joseluizcoe@gmail.com>
2020-04-09 15:05:11 -0300

    Updating build folder

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b40e21c5228c6979667875e8b4e395e2.js"
+  "/precache-manifest.ed343f8d0825d65f3253fd21f6289b01.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
