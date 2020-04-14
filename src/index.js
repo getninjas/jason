63dd2c72ad872c0a714d3db62caf558a9b6dec86
Jose Luiz Coe <joseluizcoe@gmail.com>
2020-04-14 16:41:53 -0300

    Passing event through component's properties.

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ export default class Jason {
         name={this.name}
         action={this.action}
         onReady={ this.onReady }
-        handleZipcodeExternalLinkClick={ this.handleZipcodeExternalLinkClick }
+        handleZipcodeExternalLinkClick={ (event) => this.handleZipcodeExternalLinkClick(event) }
         onZipcodeFetchSuccess={ (data) => { this.onZipcodeFetchSuccess(data); } }
         onZipcodeFetchError={ (data) => { this.onZipcodeFetchError(data); } }
         onSubmit={ this.onSubmit }
